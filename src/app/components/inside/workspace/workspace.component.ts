d9fd04eb8c5bd7f8be5fa74e9deabf65875ccb21
Souliman <souleymane.sada@gmail.com>
2023-02-12 15:37:23 +0000

    cleaning

--- src/app/components/inside/workspace/workspace.component.ts
+++ src/app/components/inside/workspace/workspace.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { map } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { DataService } from 'src/app/services/data.service';
 
