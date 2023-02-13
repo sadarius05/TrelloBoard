03ebe0c35af080919bb556c48cd7ff324e1f3a3c
Souliman <souleymane.sada@gmail.com>
2023-02-13 16:57:21 +0000

    WIP adding CRUD functions

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -31,4 +31,113 @@ export class DataService {
     console.log('boards: ', boards.data);
     return boards.data || [];
   }
+  //CRUD Board
+
+  async getBoardInfo(boardID: string) {
+    return await this.supabase
+      .from(BOARDS_TABLE)
+      .select('*')
+      .match({ id: boardID })
+      .single();
+  }
+  async updateBoard(board: any) {
+    return await this.supabase
+      .from(BOARDS_TABLE)
+      .update(board)
+      .match({ id: board.id });
+  }
+  async deleteBoard(board: any) {
+    return await this.supabase
+      .from(BOARDS_TABLE)
+      .delete()
+      .match({ id: board.id });
+  }
+
+  // CRUD Lists
+  async getBoardLists(boardId: string) {
+    const lists = await this.supabase
+      .from(LISTS_TABLE)
+      .select('*')
+      .eq('board_id', boardId)
+      .order('position');
+
+    return lists.data || [];
+  }
+
+  async addBoardList(boardId: string, position = 0) {
+    return await this.supabase
+      .from(LISTS_TABLE)
+      .insert({ board_id: boardId, position, title: 'New List' })
+      .select('*')
+      .single();
+  }
+
+  async updateBoardList(list: any) {
+    return await this.supabase
+      .from(LISTS_TABLE)
+      .update(list)
+      .match({ id: list.id });
+  }
+
+  async deleteBoardList(list: any) {
+    return await this.supabase
+      .from(LISTS_TABLE)
+      .delete()
+      .match({ id: list.id });
+  }
+
+  // CRUD Cards
+  async addListCard(listId: string, boardId: string, position = 0) {
+    return await this.supabase
+      .from(CARDS_TABLE)
+      .insert({ board_id: boardId, list_id: listId, position })
+      .select('*')
+      .single();
+  }
+
+  async getListCards(listId: string) {
+    const lists = await this.supabase
+      .from(CARDS_TABLE)
+      .select('*')
+      .eq('list_id', listId)
+      .order('position');
+
+    return lists.data || [];
+  }
+
+  async updateCard(card: any) {
+    return await this.supabase
+      .from(CARDS_TABLE)
+      .update(card)
+      .match({ id: card.id });
+  }
+
+  async deleteCard(card: any) {
+    return await this.supabase
+      .from(CARDS_TABLE)
+      .delete()
+      .match({ id: card.id });
+  }
+  // Invite others
+  async addUserToBoard(boardId: string, email: string) {
+    const user = await this.supabase
+      .from(USERS_TABLE)
+      .select('id')
+      .match({ email })
+      .single();
+
+    if (user.data?.id) {
+      const userId = user.data.id;
+      const userBoard = await this.supabase.from(USER_BOARDS_TABLE).insert(
+        {
+          user_id: userId,
+          board_id: boardId,
+        },
+        { returning: 'minimal' }
+      );
+      return userBoard;
+    } else {
+      return null;
+    }
+  }
 }
